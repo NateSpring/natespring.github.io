8a2ff88cf427b8eeeeafaefd26be9db4537a2b33
Nate Spring <45617450+NateSpring@users.noreply.github.com>
2019-07-01 06:39:02 -0700

    Add files via upload
    7/1/19: Completely overhauled the header section. 
    Removed resume button and personal information from header. 
    Replaced with logo and "scroll down" button.

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -197,3 +197,9 @@ function isExists(elem){
 	}
 	return false;
 }
+$(function() {
+	$('a[href*=#]').on('click', function(e) {
+	  e.preventDefault();
+	  $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+	});
+  });
